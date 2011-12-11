b3bdc806f1c88a9ab0b36492886c52fbfb178659
hurlebouc <hurlebouc@eb032535-49dc-4373-a4b2-a5adefcad844>
2011-12-11 12:00:40 +0000

    git-svn-id: svn+ssh://svn.code.sf.net/p/desassembleur/code/trunk@8 eb032535-49dc-4373-a4b2-a5adefcad844

--- nbproject/Package-Debug_Mac.bash
+++ nbproject/Package-Debug_Mac.bash
@@ -12,9 +12,9 @@ CND_DISTDIR=dist
 CND_BUILDDIR=build
 NBTMPDIR=${CND_BUILDDIR}/${CND_CONF}/${CND_PLATFORM}/tmp-packaging
 TMPDIRNAME=tmp-packaging
-OUTPUT_PATH=${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/desassemblage
-OUTPUT_BASENAME=desassemblage
-PACKAGE_TOP_DIR=desassemblage/
+OUTPUT_PATH=${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/desassembleur-code
+OUTPUT_BASENAME=desassembleur-code
+PACKAGE_TOP_DIR=desassembleur-code/
 
 # Functions
 function checkReturnCode
@@ -59,15 +59,15 @@ mkdir -p ${NBTMPDIR}
 
 # Copy files and create directories and links
 cd "${TOP}"
-makeDirectory "${NBTMPDIR}/desassemblage/bin"
+makeDirectory "${NBTMPDIR}/desassembleur-code/bin"
 copyFileToTmpDir "${OUTPUT_PATH}" "${NBTMPDIR}/${PACKAGE_TOP_DIR}bin/${OUTPUT_BASENAME}" 0755
 
 
 # Generate tar file
 cd "${TOP}"
-rm -f ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/package/desassemblage.tar
+rm -f ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/package/desassembleur-code.tar
 cd ${NBTMPDIR}
-tar -vcf ../../../../${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/package/desassemblage.tar *
+tar -vcf ../../../../${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/package/desassembleur-code.tar *
 checkReturnCode
 
 # Cleanup
