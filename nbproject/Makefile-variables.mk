a5a6f3c387d3a24f6bd6163921ef7d8dc072c156
hurlebouc <hurlebouc@eb032535-49dc-4373-a4b2-a5adefcad844>
2011-12-11 12:01:04 +0000

    git-svn-id: svn+ssh://svn.code.sf.net/p/desassembleur/code/trunk@9 eb032535-49dc-4373-a4b2-a5adefcad844

--- nbproject/Makefile-variables.mk
+++ nbproject/Makefile-variables.mk
@@ -9,35 +9,35 @@ CND_DISTDIR=dist
 # Debug_Mac configuration
 CND_PLATFORM_Debug_Mac=GNU-MacOSX
 CND_ARTIFACT_DIR_Debug_Mac=dist/Debug_Mac/GNU-MacOSX
-CND_ARTIFACT_NAME_Debug_Mac=desassemblage
-CND_ARTIFACT_PATH_Debug_Mac=dist/Debug_Mac/GNU-MacOSX/desassemblage
+CND_ARTIFACT_NAME_Debug_Mac=desassembleur-code
+CND_ARTIFACT_PATH_Debug_Mac=dist/Debug_Mac/GNU-MacOSX/desassembleur-code
 CND_PACKAGE_DIR_Debug_Mac=dist/Debug_Mac/GNU-MacOSX/package
-CND_PACKAGE_NAME_Debug_Mac=desassemblage.tar
-CND_PACKAGE_PATH_Debug_Mac=dist/Debug_Mac/GNU-MacOSX/package/desassemblage.tar
+CND_PACKAGE_NAME_Debug_Mac=desassembleur-code.tar
+CND_PACKAGE_PATH_Debug_Mac=dist/Debug_Mac/GNU-MacOSX/package/desassembleur-code.tar
 # Release_Mac configuration
 CND_PLATFORM_Release_Mac=GNU-MacOSX
 CND_ARTIFACT_DIR_Release_Mac=dist/Release_Mac/GNU-MacOSX
-CND_ARTIFACT_NAME_Release_Mac=desassemblage
-CND_ARTIFACT_PATH_Release_Mac=dist/Release_Mac/GNU-MacOSX/desassemblage
+CND_ARTIFACT_NAME_Release_Mac=desassembleur-code
+CND_ARTIFACT_PATH_Release_Mac=dist/Release_Mac/GNU-MacOSX/desassembleur-code
 CND_PACKAGE_DIR_Release_Mac=dist/Release_Mac/GNU-MacOSX/package
-CND_PACKAGE_NAME_Release_Mac=desassemblage.tar
-CND_PACKAGE_PATH_Release_Mac=dist/Release_Mac/GNU-MacOSX/package/desassemblage.tar
+CND_PACKAGE_NAME_Release_Mac=desassembleur-code.tar
+CND_PACKAGE_PATH_Release_Mac=dist/Release_Mac/GNU-MacOSX/package/desassembleur-code.tar
 # Debug_Linux configuration
 CND_PLATFORM_Debug_Linux=GNU-Linux-x86
 CND_ARTIFACT_DIR_Debug_Linux=dist/Debug_Linux/GNU-Linux-x86
-CND_ARTIFACT_NAME_Debug_Linux=desassemblage
-CND_ARTIFACT_PATH_Debug_Linux=dist/Debug_Linux/GNU-Linux-x86/desassemblage
+CND_ARTIFACT_NAME_Debug_Linux=desassembleur-code
+CND_ARTIFACT_PATH_Debug_Linux=dist/Debug_Linux/GNU-Linux-x86/desassembleur-code
 CND_PACKAGE_DIR_Debug_Linux=dist/Debug_Linux/GNU-Linux-x86/package
-CND_PACKAGE_NAME_Debug_Linux=desassemblage.tar
-CND_PACKAGE_PATH_Debug_Linux=dist/Debug_Linux/GNU-Linux-x86/package/desassemblage.tar
+CND_PACKAGE_NAME_Debug_Linux=desassembleur-code.tar
+CND_PACKAGE_PATH_Debug_Linux=dist/Debug_Linux/GNU-Linux-x86/package/desassembleur-code.tar
 # Release_Linux configuration
 CND_PLATFORM_Release_Linux=GNU-Linux-x86
 CND_ARTIFACT_DIR_Release_Linux=dist/Release_Linux/GNU-Linux-x86
-CND_ARTIFACT_NAME_Release_Linux=desassemblage
-CND_ARTIFACT_PATH_Release_Linux=dist/Release_Linux/GNU-Linux-x86/desassemblage
+CND_ARTIFACT_NAME_Release_Linux=desassembleur-code
+CND_ARTIFACT_PATH_Release_Linux=dist/Release_Linux/GNU-Linux-x86/desassembleur-code
 CND_PACKAGE_DIR_Release_Linux=dist/Release_Linux/GNU-Linux-x86/package
-CND_PACKAGE_NAME_Release_Linux=desassemblage.tar
-CND_PACKAGE_PATH_Release_Linux=dist/Release_Linux/GNU-Linux-x86/package/desassemblage.tar
+CND_PACKAGE_NAME_Release_Linux=desassembleur-code.tar
+CND_PACKAGE_PATH_Release_Linux=dist/Release_Linux/GNU-Linux-x86/package/desassembleur-code.tar
 #
 # include compiler specific variables
 #
