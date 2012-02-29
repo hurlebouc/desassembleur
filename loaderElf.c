f1e1836de1b7a6872a308233a5963957636a416d
jeanmil <jeanmil@eb032535-49dc-4373-a4b2-a5adefcad844>
2012-02-29 13:52:41 +0000

    ajout du loader elf
    git-svn-id: svn+ssh://svn.code.sf.net/p/desassembleur/code/trunk@94 eb032535-49dc-4373-a4b2-a5adefcad844

---
+++ loaderElf.c
@@ -0,0 +1,40 @@
+#include<stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <elf.h>
+#include <sys/mman.h>	
+#include <fcntl.h>
+
+void* loaderElf(void *debut) {
+    void* pe = debut;
+    Elf32_Ehdr *header = (Elf32_Ehdr *) debut;
+    Elf32_Shdr *shdr = (Elf32_Shdr *) (debut + header->e_shoff);
+    
+    printf("index de strtable : %d\n", header->e_shstrndx);
+    
+    char* premierchar = (char *) (debut + shdr[header->e_shstrndx].sh_offset);
+    int length = (int) (shdr[header->e_shstrndx].sh_size)/sizeof(char);
+    printf("length : %d\n", length);
+   
+    int i = 0;
+    for (i = 0; i < header->e_shnum; i++) {
+        char* name = malloc(sizeof(char) * 5);
+        printf("le shname : %d\n", shdr[i].sh_name);
+        int k = 0;
+        for(k = 0 ; k < 5 ; k++){
+            name[k] = premierchar[shdr[i].sh_name + k];
+        }
+        printf("le vrai nom %s\n", name);
+        if(strcmp(name, ".text")  == 0){
+            pe += shdr[i].sh_offset;
+            printf("offset du .text : %d\n", shdr[i].sh_offset);
+        }
+    }
+    return pe;
+}
+
+
+
+
+
---
+++ loaderelf.h
@@ -0,0 +1,15 @@
+/* 
+ * File:   loaderElf.h
+ * Author: jean
+ *
+ * Created on 3 janvier 2012, 23:25
+ */
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <elf.h>
+#include <string.h>
+
+
+void* loaderElf(void *debut);
+
