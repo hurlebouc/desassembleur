fabd379a5e43bb4a31941c8b82b70a0fef8b0c49
halleyck <halleyck@eb032535-49dc-4373-a4b2-a5adefcad844>
2012-05-24 20:32:51 +0000

    Doxygenisation de graphe sauf partie propagation de constante
    git-svn-id: svn+ssh://svn.code.sf.net/p/desassembleur/code/trunk@206 eb032535-49dc-4373-a4b2-a5adefcad844

--- Graphe.h
+++ Graphe.h
@@ -105,40 +105,85 @@ extern "C" {
 //#define SAUT_COND_TERMINAL          -11
     
     extern char* ROOT;
-    
+    /**
+     * Elément d'un graphe de flow. C'est en fait un noeud qui donne l'adresse 
+     * de ses père et de ses fils.
+     */
     typedef struct _Graphe{             //45 bytes
-        unsigned long VirtualAddr;      //adresse de l'instruction lue
-        uintptr_t aif;                  // address in file
-        int8_t etat;                    // voir les macros
-        int8_t typeLiaison;             // voir les macros
-        int8_t _immediat;               // voir les macros
-        int8_t tailleInstruction;
-        int8_t recouvert;               // 1 si PAS premier byte d'une instr
-        Processeur* pool;               // propation de constantes
-        LinkedList* listeFils;
-        LinkedList* listePeres;
+        unsigned long VirtualAddr;      /*!<Adresse de l'instruction lu*/
+        uintptr_t aif;                  /*!<Adresse dans le fichier (offset)*/
+        int8_t etat;                    /*!<Etat d'analyse du noeud*/
+        int8_t typeLiaison;             /*!<Type de liaison avec les autres noeuds*/
+        int8_t _immediat;               /*!<Caractéristique particulière du noeud*/
+        int8_t tailleInstruction;       /*!<Taille de l'instruction*/
+        int8_t recouvert;               /*!<Indique si l'octet fait partie d'une instruction, vaut 1 si oui*/
+        Processeur* pool;               /*!<Pointeur vers la l'ensemble de classes d'équivalence utilisé pour la propagation des constantes*/
+        LinkedList* listeFils;          /*!<Pointeur vers les noeuds succésseurs*/
+        LinkedList* listePeres;         /*!<Pointeur vers les noeuds précédent*/
     }Graphe;
     
+    /**
+     * Crée une structure de type graphe initialisé sans caractéristiques particulière
+     * @return Le graphe crée
+     */
     Graphe* newGraphe(void);
-        
+    
+    /**
+     * 
+     * @param g
+     */
     void terminateGraphe(Graphe* g);
+    /**
+     * Efface un noeud du graphe en rompant tout les liens avec ses prédécesseurs.
+     * Cette fonction ne devrait pas être utilisée hors de cette classe car elle
+     * ne maintient pas la propriete d'accessibilite de tous les noeuds du graphe.
+     * Il vaut mieux lui référer removeLinkRec()
+     * et ses succésseurs
+     * @param[in] g Noeud à éffacer
+     */
     void terminateNoeud(Graphe* g);
     
+    /**
+     * Efface du graphe une liaison de succéssion.
+     * @param[in] pere Noeud précédent dont on enlève le noeud fils de ses succésseurs
+     * @param[in] fils Noeud suivant dont on enlève le noeud père de ses prédécésseurs
+     */
     void removeLink(Graphe* pere, Graphe* fils);
+    
+    /**
+     * Efface la liaison entre deux noeuds. Si cette liaison rends une partie du graphe
+     * inaccessible, cette fonction supprime la partie inaccessible
+     * @param[in] pere Noeud dont on veut enlever la liaison avec le noeud fils succésseur
+     * @param[in] fils Noeud dont on supprime la liaison avec le noeud père précédent
+     */
     void removeLinkRec(Graphe* pere, Graphe* fils);
+    
+    /**
+     * Ajoute une liaison entre deux noeud du graphe. 
+     * @param[in] pere Noeud auquel on ajoute le noeud fils en tant que succésseur
+     * @param[in] fils Noeud auquel on ajoute le noeud père en tant que prédécesseur
+     */
     void addLink(Graphe* pere, Graphe* fils);
     
     
+    
+    
     /**
-     * Cette fonction trouve un noeud du graphe. 
-     * Son temps de travail est en linéaire (aussi)
+     * Trouve un noeud du graphe en compléxité linaire
+     * @param Graphe à parcourir
+     * @param Virtuel addresse du noeuf à trouver
+     * @return Pointeur vers le noeud recherché si trouvé, NULL sinon.
      */
     Graphe* getNodeWithVirtualAddr(Graphe*, uintptr_t);
     
+
     /**
-     * Cette fonction trouve un noeud du graphe mais ne peut être utilisée 
-     * qu'une seule fois sur ce graphe. Par contre elle est en temps linéaire.
-     */
+     * Trouve un noeud du graphe en compléxité linaire. Cependant cette fonction
+     * ne peut être utilisé qu'une seule fois par graphe.
+     * @param Graphe à parcourir
+     * @param Virtuel addresse du noeuf à trouver
+     * @return Pointeur vers le noeud recherché si trouvé, NULL sinon.
+     */    
     Graphe* getNodeWithVirtualAddrUnique(Graphe* g, uintptr_t va);
     
     void optimizePool(Graphe*, const Processeur* initialPool);
