79b978a31151e231a7759558393ae7739ac2fa00
hurlebouc <hurlebouc@eb032535-49dc-4373-a4b2-a5adefcad844>
2012-06-01 20:21:04 +0000

    Ajout d'une nouvelle classe pour unifier registre et mémoire.
    git-svn-id: svn+ssh://svn.code.sf.net/p/desassembleur/code/trunk@249 eb032535-49dc-4373-a4b2-a5adefcad844

---
+++ variable.c
@@ -0,0 +1,10 @@
+//
+//  variable.c
+//  desassembleur
+//
+//  Created by Hubert Godfroy on 01/06/12.
+//  Copyright (c) 2012 Mines de Nancy. All rights reserved.
+//
+
+#include <stdio.h>
+#include "variable.h"
---
+++ variable.h
@@ -0,0 +1,14 @@
+//
+//  variable.h
+//  desassembleur
+//
+//  Created by Hubert Godfroy on 01/06/12.
+//  Copyright (c) 2012 Mines de Nancy. All rights reserved.
+//
+
+#ifndef desassembleur_variable_h
+#define desassembleur_variable_h
+
+
+
+#endif
